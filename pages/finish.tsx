5274a01212286fa34dee11d65854080f84a67bb7
yceffort <yceffort@gmail.com>
2019-10-19 09:05:30 +0900

    random if random

--- pages/finish.tsx
+++ pages/finish.tsx
@@ -1,62 +1,85 @@
-import { useEffect, useState, Fragment } from "react";
-import { ListGroup, ButtonToolbar, Button } from "react-bootstrap";
-import styled from "styled-components";
+import { useEffect, useState, Fragment } from 'react'
+import { ListGroup, ButtonToolbar, Button } from 'react-bootstrap'
+import styled from 'styled-components'
 
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV !== 'production'
 const host = dev
-  ? "http://localhost:8080"
-  : "https://itm-mna-yceffort.herokuapp.com";
+  ? 'http://localhost:8080'
+  : 'https://itm-mna-yceffort.herokuapp.com'
 
 const BodyContainer = styled.div`
   margin: 10px 10px 10px 10px;
-`;
+`
+
+function shuffle(array: Array<any>) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex
+
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+
+  return array
+}
 
 export default function Finish() {
-  const [right, setRight] = useState(0);
-  const [wrong, setWrong] = useState(0);
-  const [wrongProblem, setWrongProblem] = useState([] as any[]);
+  const [right, setRight] = useState(0)
+  const [wrong, setWrong] = useState(0)
+  const [wrongProblem, setWrongProblem] = useState([] as any[])
 
   useEffect(() => {
     const getResult = async () => {
-      let right = JSON.parse(window.sessionStorage.getItem("right") || "[]");
-      let wrong = JSON.parse(window.sessionStorage.getItem("wrong") || "[]");
+      let right = JSON.parse(window.sessionStorage.getItem('right') || '[]')
+      let wrong = JSON.parse(window.sessionStorage.getItem('wrong') || '[]')
 
-      setRight(right.length);
-      setWrong(wrong.length);
+      setRight(right.length)
+      setWrong(wrong.length)
 
-      let wrongProblems = [];
+      let wrongProblems = []
 
       for (let i of wrong) {
-        let chapter = i.split("-")[0];
-        let no = i.split("-")[1];
+        let chapter = i.split('-')[0]
+        let no = i.split('-')[1]
 
         const response = await fetch(
-          `${host}/api/chapter${chapter}?limit=100&q=no:=${no}`
-        );
-        const result = await response.json();
-        let q = result[0];
-        q.chapter = chapter;
-        wrongProblems.push(q);
+          `${host}/api/chapter${chapter}?limit=100&q=no:=${no}`,
+        )
+        const result = await response.json()
+        let q = result[0]
+        q.chapter = chapter
+        wrongProblems.push(q)
       }
 
-      setWrongProblem(wrongProblems);
-    };
+      setWrongProblem(wrongProblems)
+    }
 
-    getResult();
-  }, []);
+    getResult()
+  }, [])
 
   const goReTest = () => {
-    let wrong = JSON.parse(window.sessionStorage.getItem("wrong") || "[]");
-    window.sessionStorage.clear();
-    window.sessionStorage.setItem("quiz", JSON.stringify(wrong));
-    window.location.href = `/quiz/${wrong[0].split("-")[0]}/${
-      wrong[0].split("-")[1]
-    }`;
-  };
+    let wrong = JSON.parse(window.sessionStorage.getItem('wrong') || '[]')
+
+    if (window.sessionStorage.getItem('random')) {
+      wrong = shuffle(wrong)
+    }
+
+    window.sessionStorage.clear()
+    window.sessionStorage.setItem('quiz', JSON.stringify(wrong))
+    window.location.href = `/quiz/${wrong[0].split('-')[0]}/${
+      wrong[0].split('-')[1]
+    }`
+  }
 
   const goHome = () => {
-    window.location.href = "/";
-  };
+    window.location.href = '/'
+  }
 
   return (
     <BodyContainer>
@@ -87,17 +110,17 @@ export default function Finish() {
               <h5>
                 {chapter}-{no}.{question}
               </h5>
-              {answer_type === "multiple_essay" ? (
+              {answer_type === 'multiple_essay' ? (
                 <pre>{answer.toString()}</pre>
               ) : (
                 <pre>{answer}</pre>
               )}
             </Fragment>
-          );
-        }
+          )
+        },
       )}
     </BodyContainer>
-  );
+  )
 }
 
 // Finish.getInitialProps = async function({
